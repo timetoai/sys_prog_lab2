10494ffdb5f99df11d3c57da1e7f660da1dc17ec
root <root@localhost.localdomain>
2019-11-12 21:27:47 +0300

    Improved client interaction module

--- lab2client.C
+++ lab2client.C
@@ -4,12 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
+#include "message.h"
 
-char msg1[] = "Hello there!\n";
-char msg2[] = "Bye bye!\n";
+struct message *msg;
+char *f, *mac;
+void atexit_handler();
 
 int main()
 {
+    //Обработка завершения программы
+    atexit(atexit_handler);
+
     int sock;
     struct sockaddr_in addr;
 
@@ -23,13 +29,49 @@ int main()
     addr.sin_family = AF_INET;
     addr.sin_port = htons(3425);
     addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-    sendto(sock, msg1, sizeof(msg1), 0,
-           (struct sockaddr *)&addr, sizeof(addr));
+    msg = (struct message*) malloc(7);
+    mac = (char*) malloc(15);
+    f = (char*) malloc(1);
 
-    connect(sock, (struct sockaddr *)&addr, sizeof(addr));
-    send(sock, msg2, sizeof(msg2), 0);
+    while(1)
+    {
+        fputs("Enter mac address in format ****:****:****\n", stdout);
+        fgets(mac, 15, stdin);
+        bool valid = true;
+        for (int i = 0; i < 11 ; i+=5)
+        {
+            for (int j = 0; j < 4; ++j)
+            {
+                if (!isxdigit(mac[i + j])) 
+                {
+                    valid = false;
+                    break;
+                }
+            }
+            if (!valid) {break;}
+        }
+        if (!valid) {fputs("Incorrect mac address\n", stdout); continue;}
+
+        fputs("Enter flag (ADD 0, DELETE 1, CHECK 2): ", stdout);
+        fgets(f, 1, stdin);
+        if (!(isdigit(*f) || atoi(f) < 2)) {fputs("Incorrect flag\n", stdout); continue;}
+
+        msg->mac_addr[0] = strtol(mac, NULL, 16);
+        msg->mac_addr[1] = strtol(mac + 5, NULL, 16);
+        msg->mac_addr[2] = strtol(mac + 10, NULL, 16);
+        msg->flag = atoi(f);
+        printf("Sending...\n");
+        sendto(sock, (void*) msg, sizeof(msg), 0, (struct sockaddr *)&addr, sizeof(addr));
+    }
 
     close(sock);
 
     return 0;
+}
+
+void atexit_handler()
+{
+    free(mac);
+    free(f);
+    free(msg);
 }
--- lab2server.C
+++ lab2server.C
@@ -3,36 +3,133 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <cstring>
+#include <unistd.h>
+#include "message.h"
 
-int main()
+struct message **msgs;
+static const char *optString = "wdl:a:p:vh";
+int l2wait = 0;
+char *l2logfile = NULL;
+char *l2addr = NULL;
+int l2port = 3425;
+
+void atexit_handler();
+void display_usage();
+void exit_signal_handler(int signum);
+void log_signal_handler(int signum);
+
+int main(int argc, char **argv)
 {
+	//Обработка завершения программы
+	atexit(atexit_handler);
+
+    //Обработка сигналов
+    struct sigaction act;
+    sigset_t sigset;
+
+    sigemptyset(&sigset);
+    sigaddset(&sigset, SIGINT);
+    sigaddset(&sigset, SIGTERM); 
+    sigaddset(&sigset, SIGQUIT);
+    sigaddset(&sigset, SIGUSR1);
+
+    memset(&act, 0, sizeof(act));
+	act.sa_handler = exit_signal_handler;
+	act.sa_mask = sigset;
+	sigaction(SIGINT, &act, 0);
+	sigaction(SIGTERM, &act, 0);
+	sigaction(SIGQUIT, &act, 0);
+
+	memset(&act, 0, sizeof(act));
+	act.sa_handler = log_signal_handler;
+	act.sa_mask = sigset;
+	sigaction(SIGUSR1, &act, 0);
+
+	sigprocmask(SIG_UNBLOCK, &sigset, NULL);
+
+	//Обработка ключей
+	int opt = getopt(argc, argv, optString);
+	while(opt != -1)
+	{
+		switch(opt)
+		{
+			case 'w':
+				break;
+			case 'd':
+				break;
+			case 'l':
+				break;
+			case 'a':
+				break;
+			case 'p':
+				break;
+			case 'v':
+				printf("lab2server version 1.0");
+				exit(0);
+			case 'h':
+			default:
+				display_usage();
+		}
+		opt = getopt(argc, argv, optString);
+	}
+
+	//Подготовка сокета к работе
 	int sock;
-	    struct sockaddr_in addr;
-	    char buf[1024];
-	    int bytes_read;
+	struct sockaddr_in addr;
+	struct message *msg = (struct message*) malloc(7);
+	int bytes_read;
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
 
-	    sock = socket(AF_INET, SOCK_DGRAM, 0);
-	    if(sock < 0)
-	    {
-	        perror("socket");
-	        exit(1);
-	    }
+    //Считывание сообщений
+    if(sock < 0)
+    {
+        perror("socket");
+        exit(1);
+    }
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(3425);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	    addr.sin_family = AF_INET;
-	    addr.sin_port = htons(3425);
-	    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	    if(bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-	    {
-	        perror("bind");
-	        exit(2);
-	    }
+    if(bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("bind");
+        exit(2);
+    }
 
-	    while(1)
-	    {
-	        bytes_read = recvfrom(sock, buf, 1024, 0, NULL, NULL);
-	        buf[bytes_read] = '\0';
-	        printf(buf);
-	    }
+    while(1)
+    {
+        bytes_read = recvfrom(sock, (void *) msg, 7, 0, NULL, NULL);
+        if (msg->flag > 2)
+        {
+        	printf("Got incorrect flag '%d'", msg->flag);
+        	continue;
+        }
+        printf("Received MAC: %x:%x:%x flag=%d\n", msg->mac_addr[0], msg->mac_addr[1], msg->mac_addr[2], msg->flag);
+    }
 
-	    return 0;
+    return 0;
+}
+
+void display_usage()
+{
+	printf("Displaying usage\n");
+	exit(0);
+}
+
+void atexit_handler()
+{
+	
+}
+
+void exit_signal_handler(int signum)
+{
+	printf("Exit by signal with number %d\n", signum);
+	exit(0);
+}
+
+void log_signal_handler(int signum)
+{
+	printf("logging...\n");
 }
